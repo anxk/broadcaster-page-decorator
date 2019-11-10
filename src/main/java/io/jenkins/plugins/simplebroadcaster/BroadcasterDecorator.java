4d7c17ca89532d0b65a6c0bf7fb897360b2b380b
anxk <xiaokang112358@gmail.com>
2019-11-10 23:23:43 +0800

    update

--- src/main/java/io/jenkins/plugins/simplebroadcaster/BroadcasterDecorator.java
+++ src/main/java/io/jenkins/plugins/simplebroadcaster/BroadcasterDecorator.java
@@ -1,21 +1,69 @@
 package io.jenkins.plugins.simplebroadcaster;
 
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
 import hudson.Extension;
 import hudson.model.PageDecorator;
+import net.sf.json.JSONObject;
 
 @Extension
 public class BroadcasterDecorator extends PageDecorator {
 
+    public final static String DEFAULT_COLOR = "white";
+    public final static String DEFAULT_BACKGROUND = "red";
+
+    private String message;
+    private String color;
+    private String background;
+
+    @DataBoundConstructor
+    public BroadcasterDecorator(String message, String color, String background) {
+        this();
+        this.message = message;
+        this.color = color;
+        this.background = background;
+    }
+
+    public BroadcasterDecorator() {
+        load();
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        req.bindJSON(this, json);
+        save();
+        return true;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public String getMessage() {
-        return BroadCasterManager.get().getMessage();
+        return message;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public String getColor() {
-        return BroadCasterManager.get().getColor();
+        if (color.equals("")) {
+            return DEFAULT_COLOR;
+        }
+        return color;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
     }
 
     public String getBackground() {
-        return BroadCasterManager.get().getBackground();
+        if (background.equals("")) {
+            return DEFAULT_BACKGROUND;
+        }
+        return background;
     }
 
 }
